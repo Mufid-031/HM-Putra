45c101820ffc9bb5c60af59614623cc7605d73fb
Mufid-031 <mufidmaxsus17@gmail.com>
2024-12-08 00:47:16 +0700

    tampilan awal

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,197 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+// pemanggil class
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  // constuctor wajib
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Instagram",
+            style: TextStyle(
+                fontSize: 15, color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+          actions: const [
+            IconButton(
+              onPressed: ButtonAction.new,
+              icon: Icon(Icons.heart_broken_outlined),
+              color: Colors.white,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            IconButton(
+              onPressed: ButtonAction.new,
+              icon: Icon(Icons.message),
+              color: Colors.white,
             ),
           ],
+          backgroundColor: Colors.black,
+        ),
+        body: Container(
+          color: Colors.black,
+          padding: const EdgeInsets.all(10.0),
+          child: const Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  IconButton(
+                    onPressed: ButtonAction.new,
+                    icon: Icon(Icons.person),
+                    color: Colors.white,
+                    iconSize: 75,
+                    style: ButtonStyle(
+                        shape: WidgetStatePropertyAll(
+                          CircleBorder(
+                            side: BorderSide(color: Colors.white),
+                          ),
+                        )),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          IconButton(
+                            onPressed: ButtonAction.new,
+                            icon: Icon(Icons.person),
+                            color: Colors.white,
+                          ),
+                          Text(
+                            "Mufid Risqi",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 12,
+                            ),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                  IconButton(
+                    onPressed: ButtonAction.new,
+                    icon: Icon(Icons.keyboard_option_key),
+                    color: Colors.white,
+                  )
+                ],
+              ),
+              Image(
+                  image: NetworkImage(
+                      'https://palcomtech.ac.id/wp-content/uploads/2023/10/60bb4a2e143f632da3e56aea_Flutter-app-development-2.png')),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      Row(
+                        children: [
+                          IconButton(
+                            onPressed: ButtonAction.new,
+                            icon: Icon(Icons.heart_broken),
+                            color: Colors.white,
+                          ),
+                          Text(
+                            "1.555",
+                            style: TextStyle(color: Colors.white),
+                          )
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          IconButton(
+                            onPressed: ButtonAction.new,
+                            icon: Icon(Icons.comment_rounded),
+                            color: Colors.white,
+                          ),
+                          Text(
+                            "200",
+                            style: TextStyle(color: Colors.white),
+                          )
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          IconButton(
+                            onPressed: ButtonAction.new,
+                            icon: Icon(Icons.share_rounded),
+                            color: Colors.white,
+                          ),
+                          Text(
+                            "168",
+                            style: TextStyle(color: Colors.white),
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                  IconButton(
+                    onPressed: ButtonAction.new,
+                    icon: Icon(Icons.bookmark_border),
+                    color: Colors.white,
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+        bottomNavigationBar: const BottomAppBar(
+          color: Colors.black,
+          child: Row(
+            children: [
+              Expanded(
+                child: IconButton(
+                  onPressed: ButtonAction.new,
+                  icon: Icon(Icons.home),
+                  color: Colors.white,
+                ),
+              ),
+              Expanded(
+                child: IconButton(
+                  onPressed: ButtonAction.new,
+                  icon: Icon(Icons.search),
+                  color: Colors.white,
+                ),
+              ),
+              Expanded(
+                child: IconButton(
+                  onPressed: ButtonAction.new,
+                  icon: Icon(Icons.add_box_outlined),
+                  color: Colors.white,
+                ),
+              ),
+              Expanded(
+                child: IconButton(
+                  onPressed: ButtonAction.new,
+                  icon: Icon(Icons.video_collection_outlined),
+                  color: Colors.white,
+                ),
+              ),
+              Expanded(
+                child: IconButton(
+                  onPressed: ButtonAction.new,
+                  icon: Icon(Icons.person),
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+class ButtonAction {
+  void clicked() {
+    print("Hello");
   }
 }
